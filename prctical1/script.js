672db9d9d5c111810750a8c77c9489e54100a9f3
Madhur <madhur.wanjari@gmail.com>
2026-07-14 10:22:34 +0530

    Create script.js

---
+++ prctical1/script.js
@@ -0,0 +1,13 @@
+ function greet(){
+       alert("Welcome to JavaScript Programming!");
+
+       console.table([
+       {
+        Name : "Sample User",
+        Course : "JavaScript"
+       }
+
+       ]);
+    console.time("Execution");
+    console.timeEnd("Execution");
+    }
